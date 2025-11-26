7cad410d460aa1240fde1c2076dde4c46e8e080f
최재영 <chlwodud9807@naver.com>
2025-11-26 10:09:38 +0900

    Step 1

--- Sources/MyFirstLibrary/MyFirstLibrary.swift
+++ Sources/MyFirstLibrary/MyFirstLibrary.swift
@@ -1,2 +1,15 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+struct Person {
+    let name: String
+    let age: Int
+}
+
+extension Date {
+    var year: Int {
+        return Calendar.current.component(.year, from: Date())
+    }
+}
